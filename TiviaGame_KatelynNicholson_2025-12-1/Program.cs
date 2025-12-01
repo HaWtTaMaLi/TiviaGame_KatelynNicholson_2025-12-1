7a2495cf48f9f284aaea0a85064c5cfac6825b19
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-12-01 09:43:07 -0400

    push

--- TiviaGame_KatelynNicholson_2025-12-1/Program.cs
+++ TiviaGame_KatelynNicholson_2025-12-1/Program.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
 
 namespace TiviaGame_KatelynNicholson_2025_12_1
 {
     internal class Program
     {
+        //prompt to enter name
+        //answer 10 questions
+        //answer by 1, 2, 3 or 4. must be 4 choices for every question
+        //any other keys not 1, 2, 3, 4 promt choose a proper answer
+        //after answering all questions at the end prompt show score 
+        //with a message based on performance
+        //after final score user is prompt, play again or quit (Y or N)
+        //Hud, playerName, current question #, amount guessed right /out of amount answered as a %
+        //make it easy to add and remove questions, no hardcoding.
+        //loops data structures arrays and lists
+        //is your solution scalable
+        //no magic numbers
+        //methods for common functionality, no duplicated code (if code needs to be written twice stick it in a method
+        //EXTRA
+        //COLOURS, ASCII art, formatting make it look good
+        //add an easter egg? a hidden secret? (include note when submitting if you added an easter egg
+
+        //https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.dictionary-2?view=net-10.0
+
+        //create dictionary
+        Dictionary<string, string> questions = new Dictionary<string, string>();
+        //questions are KEY and answer is Value?
+
         static void Main()
         {
 
